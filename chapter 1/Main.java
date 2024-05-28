6ff9e66cb9203197d142b24bef80f17055b25503
dhirendraxd <dhirendraxd@gmail.com>
2024-05-28 19:19:58 +0545

    new changes

--- chapter 1/Main.java
+++ chapter 1/Main.java
@@ -1,6 +1,7 @@
 
-public class Main {
-    public static void main(String[] args) {
+public class Main { // thsi is a class 
+    public static void main(String[] args) { // this is a function
         System.out.println("hello world");
     }
 }
+ 
