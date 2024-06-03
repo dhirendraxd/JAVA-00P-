6119bfd1df7e7d5ed9af4e19d3cb42eb8283ab0c
dhirendraxd <dhirendraxd@gmail.com>
2024-06-03 18:13:17 +0545

    initialized gradle for java

---
+++ src/main/java/Literals.java
@@ -0,0 +1,7 @@
+package src.main.java;
+public class Literals {
+    public static void main(String[] args) {
+        int age = 34;
+        
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+package src.main.java;
 
 public class Main { // thsi is a class 
     
