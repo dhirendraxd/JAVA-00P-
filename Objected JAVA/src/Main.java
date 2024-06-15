936c071181bc417ad04223a054ce800fb2b0e6eb
dhirendraxd <dhirendraxd@gmail.com>
2024-06-15 17:28:25 +0545

    new changes

--- Objected JAVA/src/Main.java
+++ Objected JAVA/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main { // thsi is a class 
     
     public static void main(String[] args) { // this is a function
@@ -8,6 +10,14 @@ public class Main { // thsi is a class
         int sum = num1 + num2 +num3;
         System.out.println(sum); // println addsup an new line init and formating by itself 
         System.out.print(sum); // we can also use print but it doesnt provide a new line after it it prints in its own line 
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println("dhiren");
+        Scanner sc = new Scanner(System.in);
+        String st =  sc.next();
+        System.out.println(st);
     }
 }
 
--- Objected JAVA/src/strings.java
+++ Objected JAVA/src/strings.java
@@ -1,9 +1,16 @@
-import java.util.Scanner;
 public class strings {
     public static void main(String[] args) {
+        String string = "Hey how you doin";
+        System.out.println(string);
+           // Using string literals
+           String str1 = "Hello, World!";
         
-        System.out.println("dhiren");
-        Scanner sc = new Scanner(System.in);
-        String st =  sc.next();
+           // Using the String class constructor
+           String str2 = new String("Hello, Java!");
+           
+           // Printing the strings
+           System.out.println(str1);
+           System.out.println(str2);
     }
+
 }
