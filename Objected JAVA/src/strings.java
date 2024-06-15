b5da1a7554e529be0428f0c6ad35ae92acebf66d
dhirendraxd <dhirendraxd@gmail.com>
2024-06-15 17:10:15 +0545

    Remove unused files and directories

--- Objected JAVA/src/strings.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class strings {
-    public static void main(String[] args) {
-        
-        System.out.println("dhiren");
-        Scanner sc = new Scanner(System.in);
-        String st =  sc.next();
-    }
-}
