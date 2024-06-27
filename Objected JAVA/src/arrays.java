6e91f459eddb0183152c93cb7bb6999a59244cf1
dhirendraxd <dhirendraxd@gmail.com>
2024-06-27 08:29:41 +0545

    arrays

---
+++ Objected JAVA/src/arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+    
+    public static void main(String[] args) {
+        // array of 5000 entity
+        int [] marks = new int[5];
+
+        // we declare a ARRAY like above and we  create a new int with  numbers of data to be stored 
+
+        int [] thrdway ={23, 4, 43, 523, 523};
+        for (int i = 0; i < thrdway.length; i++) {
+            System.out.println(thrdway[i]);
+        }
+    }
+}
+ 
---
+++ Objected JAVA/src/foreach.java
@@ -0,0 +1,12 @@
+public class foreach {
+    public static void main(String[] args) {
+        int [] marks = {753, 534, 53, 523, 5242, 232};
+        System.out.println(marks.length);
+        //output : 6 // total numbers inside the array of marks 
+
+        String [] studnets = {"dhiren", "clashe"};
+        for (int i = 0; i < studnets.length; i++) {
+            System.out.println(studnets[i]);
+        }
+    }
+}
