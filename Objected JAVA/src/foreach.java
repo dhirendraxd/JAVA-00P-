f8222ada8a93136b6139e0c9d865e43d24831eb4
dhirendraxd <dhirendraxd@gmail.com>
2024-06-28 06:35:53 +0545

    for each loop in java

--- Objected JAVA/src/foreach.java
+++ Objected JAVA/src/foreach.java
@@ -3,9 +3,19 @@ public class foreach {
         int [] marks = {753, 534, 53, 523, 5242, 232};
         System.out.println(marks.length);
         //output : 6 // total numbers inside the array of marks 
+        for(int element: marks){
+            System.out.println(element);
+        }
+        //the element written is an variables that stores the values of the elements extracted from the marks  and print and same for next one till the end 
+        // same goes for strings and  float we just change the int to string or float in each of them ..
+
 
         String [] studnets = {"dhiren", "clashe"};
         for (int i = 0; i < studnets.length; i++) {
+            System.out.println(studnets[i]);
+        }
+        //reversing an array 
+        for (int i = studnets.length - 1; i >=0; i--) {
             System.out.println(studnets[i]);
         }
     }
---
+++ Objected JAVA/src/rev_string.java
@@ -0,0 +1,11 @@
+public class rev_string {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println("Original string: " + str);
+        
+        System.out.print("Reversed string: ");
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+    }
+}
