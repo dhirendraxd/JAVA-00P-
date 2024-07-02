b762f8afa647233745b97b009c009ab025431df3
dhirendraxd <dhirendraxd@gmail.com>
2024-07-02 09:32:35 +0545

    new chages

--- Objected JAVA/src/oop/oop.java
+++ Objected JAVA/src/oop/oop.java
@@ -1,5 +1,8 @@
 package oop;
 
 public class oop {
-    
+    public static void main(String[] args) {
+        System.out.println("----------");
+        System.out.println("hey this is the first program for OOP in java :\n");
+    }
 }
