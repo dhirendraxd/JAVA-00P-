7005b202e420541f83b28ee2ae8d82ac7ab5d1bf
dhirendraxd <dhirendraxd@gmail.com>
2024-07-03 21:36:32 +0545

    cleared the concept of methods and user input and passing the value by value and ref

--- Objected JAVA/src/oop/exercise.java
+++ Objected JAVA/src/oop/exercise.java
@@ -2,47 +2,66 @@ package oop;
 
 import java.util.Scanner;
 
-class tommy{ // this is a clas
-    int namebyuser;
+class Tommy{ // this is a clas
     public void hit(){ // void for ouput  // int or string for returning values 
         System.out.println("hitting the enemy");
-
+        
     }
     public void run (){
         System.out.println("running ");
-
+        
     }
     public void fire(){
         System.out.println("firing ");
     }
-    public int setvalues(int input_name){
-        return namebyuser = input_name;
+    ///////////////////////////////////////////////
+//so first we take userinput and assign it to this below method and then it pass that value to a new variabls calle  name by user and returns it 
+//.......................................................//
+///        then the new methods getnamebyuser retunrs the name by user  , in case we might thnk why not using  setvalues it alos does the same but there a diffrence cause its a setter and getnamebyuser is a geter so we mostly use it   , we use it to only take input form user and then pass the value  given in form of a new variabl and the next getter will get it then it can retunr as its main work is to return value
+/////////////////////////////////////////////
+    private int namebyuser; // empty var
+    public int setvalues(int input_name){ // the name shoudl be same for both parameter and inside var 
+        namebyuser = input_name;
+        return namebyuser;
+        // setter method to set value after user sends the value and assigning it to a new varinside this and then applying it in a new getter method
     }
 
     public int getNameByUser() {
         return namebyuser; // Getter method to retrieve 'namebyuser' variable
     }
-    
+    // inpur form user 
+// System.out.print("Enter a number for 'namebyuser': ");
+// int userInput = sc.nextInt(); // Read user input as an integer
 }
+
 public class exercise {
     public static void main(String[] args) {
-        Scanner sc.new Scanner(system.in);
-        tommy playe1 = new tommy(); 
-// when workign with methods and class we have to assign a new  entity to be store inside that as tommy player1 is a new  object which will have its own properties that are inside of the class 
+        Scanner sc = new Scanner(System.in);
+        Tommy playe1 = new Tommy(); 
 
-// basically it means that it can be a part of the class as a family and can use all the thigns inside of the class 
+// when workign with methods and class we have to assign a new  entity to be store inside that as tommy player1 is a new  object which will have its own properties that are inside of the class, basically it means that it can be a part of the class as a family and can use all the thigns inside of the class 
+
 playe1.setvalues(23);
-
+System.out.println("player 1 vlaues:"+playe1.getNameByUser());
+Tommy playe11 = new Tommy();
 
 // inpur form user 
 System.out.print("Enter a number for 'namebyuser': ");
-int userInput = scanner.nextInt(); // Read user input as an integer
 
-playe1.setvalues(userInput); // Set 'namebyuser' with user input
+int value = sc.nextInt(); // Read user input as an integer
+
+playe1.setvalues(value); // Set 'namebyuser' with user input
+System.out.println("enter the value for chutmat:");
+int chutmat = sc.nextInt();
+playe11.setvalues(chutmat);
+System.out.println("this is the entered data "+playe11.getNameByUser());
 
 
         playe1.fire();
         playe1.run();
         playe1.hit();
+        sc.close();
     }
 }
+
+
---
+++ Objected JAVA/src/oop/exercise_code_without_explainatio.java
@@ -0,0 +1,53 @@
+package oop;
+
+import java.util.Scanner;
+
+class Tommy { // Class name follows Java naming conventions
+    public void hit() {
+        System.out.println("hitting the enemy");
+    }
+
+    public void run() {
+        System.out.println("running");
+    }
+
+    public void fire() {
+        System.out.println("firing");
+    }
+
+    // Private instance variable for encapsulation
+    private int namebyuser;
+
+    // Setter method to set 'namebyuser' and return it
+    public int setValues(int input_name) {
+        namebyuser = input_name; // Set the value of 'namebyuser'
+        return namebyuser; // Return the updated value
+    }
+
+    // Getter method to retrieve 'namebyuser'
+    public int getNameByUser() {
+        return namebyuser; // Getter method to retrieve 'namebyuser' variable
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Tommy player1 = new Tommy(); 
+
+        // Set initial value directly
+        player1.setValues(23);
+        System.out.println("Player 1 initial value: " + player1.getNameByUser());
+
+        // Input from user
+        System.out.print("Enter a number for 'namebyuser': ");
+        int value = sc.nextInt(); // Read user input as an integer
+        player1.setValues(value); // Set 'namebyuser' with user input
+        System.out.println("The input data was: " + value);
+
+        // Call other methods
+        player1.fire();
+        player1.run();
+        player1.hit();
+
+        sc.close(); // Close the Scanner object
+    }
+}
