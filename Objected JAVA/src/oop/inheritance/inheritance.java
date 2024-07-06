fc36504e035aa042c8e84cfc256c97ef94852743
dhirendraxd <dhirendraxd@gmail.com>
2024-07-06 11:09:56 +0545

    inheritance

---
+++ Objected JAVA/src/oop/inheritance/inheritance.java
@@ -0,0 +1,26 @@
+package oop.inheritance ;
+
+class base{
+    int x ;
+   public int getx(){
+    return x;
+    }
+
+    public void setx(int x){
+        x = x;
+    }
+    public void printme(){
+        System.out.println("this is an cons");
+    }
+}
+class base_son extends base{
+// now this class inherits all the things and codes inside of the base to this class everthing
+
+// casuse we used base_son extends base
+
+}
+public class inheritance {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
