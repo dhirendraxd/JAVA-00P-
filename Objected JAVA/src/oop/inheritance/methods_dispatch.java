f3b42f6478f164d19468a7c154b36561fe3b40c1
dhirendraxd <dhirendraxd@gmail.com>
2024-07-09 09:58:05 +0545

    theoritical concepts

--- Objected JAVA/src/oop/inheritance/methods_dispatch.java
+++ Objected JAVA/src/oop/inheritance/methods_dispatch.java
@@ -26,7 +26,10 @@ public class methods_dispatch {
 
         phone obj = new smartphone();
         obj.on();// output :i am opening as a phone smartphone 
-
+        obj.greet();
+        // obj.swagat();// not allowed
+        // must be overridden methods to access using obj. 
+        // if we wanted to acess the overrriden functio not of child but of parent we use super (), keywords  (x)parameterized for construtor 
 
 
     }
