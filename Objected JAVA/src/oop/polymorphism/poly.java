0c92fc656af8b5fca8a792e5d37290513b751469
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-08-17 21:40:25 +0545

    chore: Refactor code to implement Animal interface and override methods

---
+++ Objected JAVA/src/oop/polymorphism/poly.java
@@ -0,0 +1,35 @@
+package oop.polymorphism;
+class phone{
+    void ring(){
+        System.out.println("ringing");
+    }
+    void vibrate(){
+        System.out.println("vibrating");
+    }
+    void camera(){
+        System.out.println("taking photo");
+    }
+
+}
+
+// parent class cnat have access to the child class methods but child class can have access to the parent class methods
+class smartphone extends phone{
+    void music(){
+        System.out.println("playing music");
+    }
+    @Override
+    void camera(){
+        System.out.println("taking photo and video");
+    }
+}
+public class poly {
+    public static void main(String[] args) {
+        phone p = new smartphone();
+        p.ring();
+        p.music();// why not workign ? becuase the object is of phone class and phone class does not have the music method
+        p.camera(); // output will be taking photo and video
+       
+
+    }
+
+}
