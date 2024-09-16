ec893f7b078990125a53c3b256b8dd506619864f
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-09-16 17:55:32 +0545

    Cleaned up unused ArrayList initialization in gen.java

--- Objected JAVA/src/generics_java/gen.java
+++ Objected JAVA/src/generics_java/gen.java
@@ -4,14 +4,7 @@ import java.util.ArrayList;
 
 public class gen {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-        list.add(50);
-        // list.add("hello"); // this will give an error because we have defined the type of list as Integer    
-
+   
         
     }
 }
