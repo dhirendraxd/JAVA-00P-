a9a6c08961dbe708ea8e4e208d4273f903d7a012
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-09-16 18:22:12 +0545

    Refactor code to increase heap size in .vscode/settings.json

--- Objected JAVA/src/generics_java/gen.java
+++ Objected JAVA/src/generics_java/gen.java
@@ -2,9 +2,30 @@ package generics_java;
 
 import java.util.ArrayList;
 
+
+// we can use any name in place of T
+//  class printer <what_ever_we_want>{
+// }
+
+class printer<T>{
+        T  thinngsprint;
+    public printer(T thinngsprint){
+        this.thinngsprint = thinngsprint;
+    }
+    public void print(){
+        System.out.println(thinngsprint);
+    }
+
+
+}
+
 public class gen {
     public static void main(String[] args) {
-   
-        
+        printer<Integer> obj = new printer<>(5);
+        obj.print(); // output: 5   
+        printer<String> obj1 = new printer<>("Hello");
+        obj1.print(); // output: Hello
+        printer<ArrayList> obj2 = new printer<>(new ArrayList());
+        obj2.print(); // output: []
     }
 }
---
+++ Objected JAVA/src/generics_java/gen2.java
@@ -0,0 +1,19 @@
+package generics_java;
+import java.util.ArrayList;
+public class gen2 {
+    public static void main(String[] args) {
+        // this array list will accept anything      like integer, string, double etc but if we want to add only integer then we use gens    
+
+        ArrayList <Integer> arlst = new ArrayList();
+        arlst.add(5);
+        // arlst.add("Hello"); 
+        // we can add any type of data in arraylist but if we wnat to add only integer type then w use gens
+        // arlst.add(5.5);
+        System.out.println(arlst); // output: [5, Hello, 5.5]
+
+        int a = (int) arlst.get(2);
+        System.out.println(a); // output: 5
+
+
+    }
+}
