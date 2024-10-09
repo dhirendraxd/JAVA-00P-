c4d7a7a42f56c2675eda38b57b0ead6f3486c5b9
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-10-09 20:13:55 +0545

    Refactor gen.java to fix type errors and remove unused code

--- Objected JAVA/src/generics_java/gen.java
+++ Objected JAVA/src/generics_java/gen.java
@@ -2,10 +2,30 @@ package generics_java;
 
 import java.util.ArrayList;
 
+class mygenerics{
+    int val ;
+    private  t1 t1;
+
+}
+public void setT1ANDVAL(t1 t1 , int val){
+    this.t1 = t1;
+    this.val= val;
+
+
+}
+public int getval(){
+    return val;
+}
+public t1 gett1(){
+    return t1;
+}
+
 
 public class gen {
     public static void main(String[] args) {
         ArrayList <Integer> arlst = new ArrayList();
+        mygenerics <String> ma = mygenerics(32, "hello");
+        ma.gett1();// ouput : hello
         // arlst.add("hello");
         arlst.add(1);
         arlst.add(2.3);
