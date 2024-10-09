0254747ad20bdfa56a30bfc688711dd2556b084c
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-10-09 20:05:20 +0545

    Refactor gen.java to fix type errors and remove unused code

--- Objected JAVA/src/generics_java/gen.java
+++ Objected JAVA/src/generics_java/gen.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 public class gen {
     public static void main(String[] args) {
         ArrayList <Integer> arlst = new ArrayList();
-        arlst.add("hello");
+        // arlst.add("hello");
         arlst.add(1);
         arlst.add(2.3);
-        arlst.add('a');
+        // arlst.add('a');
         int a = arlst.get(56); // this will give an error because the index is out of bound
         System.out.println(a); // output : 1
 
