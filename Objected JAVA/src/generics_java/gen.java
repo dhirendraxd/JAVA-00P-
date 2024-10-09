f671a9af6290fd0fee35f0b1156289ef70d8943e
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-10-09 20:14:11 +0545

    Refactor gen.java to fix type errors and remove unused code

--- Objected JAVA/src/generics_java/gen.java
+++ Objected JAVA/src/generics_java/gen.java
@@ -2,36 +2,59 @@ package generics_java;
 
 import java.util.ArrayList;
 
-class mygenerics{
-    int val ;
-    private  t1 t1;
+// Generic class definition
+class MyGenerics<T> {
+    int val;
+    private T t1;
 
+    // Constructor to initialize t1 and val
+    public MyGenerics(T t1, int val) {
+        this.t1 = t1;
+        this.val = val;
+    }
+
+    // Setter for t1 and val
+    public void setT1ANDVAL(T t1, int val) {
+        this.t1 = t1;
+        this.val = val;
+    }
+
+    // Getter for val
+    public int getVal() {
+        return val;
+    }
+
+    // Getter for t1
+    public T getT1() {
+        return t1;
+    }
 }
-public void setT1ANDVAL(t1 t1 , int val){
-    this.t1 = t1;
-    this.val= val;
 
-
-}
-public int getval(){
-    return val;
-}
-public t1 gett1(){
-    return t1;
-}
-
-
-public class gen {
+public class Gen {
     public static void main(String[] args) {
-        ArrayList <Integer> arlst = new ArrayList();
-        mygenerics <String> ma = mygenerics(32, "hello");
-        ma.gett1();// ouput : hello
-        // arlst.add("hello");
-        arlst.add(1);
-        arlst.add(2.3);
-        // arlst.add('a');
-        int a = arlst.get(56); // this will give an error because the index is out of bound
-        System.out.println(a); // output : 1
+        // ArrayList that only allows Integers
+        ArrayList<Integer> arlst = new ArrayList<>();
 
+        // Create an instance of MyGenerics with String type
+        MyGenerics<String> ma = new MyGenerics<>("hello", 32);
+
+        // Get value from mygenerics instance
+        System.out.println(ma.getT1()); // Output: hello
+
+        // Adding integers to the list (2.3 would cause an error)
+        arlst.add(1);
+        // arlst.add(2.3); // Uncommenting this will cause a compilation error
+        // arlst.add('a'); // Uncommenting this will also cause a compilation error
+
+        // Correct way of adding integers
+        arlst.add(2); // Adding an integer value
+
+        // Trying to access an index out of bounds will throw an exception
+        try {
+            int a = arlst.get(56); // This will throw IndexOutOfBoundsException
+            System.out.println(a);  // This line won't be reached
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bounds!");
+        }
     }
 }
