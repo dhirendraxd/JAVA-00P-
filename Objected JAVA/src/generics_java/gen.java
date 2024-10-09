11ccb7866826292af5f9c084e4a968285e906306
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-10-09 20:04:25 +0545

    Refactor gen.java to fix type errors and remove unused code

--- Objected JAVA/src/generics_java/gen.java
+++ Objected JAVA/src/generics_java/gen.java
@@ -3,29 +3,15 @@ package generics_java;
 import java.util.ArrayList;
 
 
-// we can use any name in place of T
-//  class printer <what_ever_we_want>{
-// }
-
-class printer<T>{
-        T  thinngsprint;
-    public printer(T thinngsprint){
-        this.thinngsprint = thinngsprint;
-    }
-    public void print(){
-        System.out.println(thinngsprint);
-    }
-
-
-}
-
 public class gen {
     public static void main(String[] args) {
-        printer<Integer> obj = new printer<>(5);
-        obj.print(); // output: 5   
-        printer<String> obj1 = new printer<>("Hello");
-        obj1.print(); // output: Hello
-        printer<ArrayList> obj2 = new printer<>(new ArrayList());
-        obj2.print(); // output: []
+        ArrayList <Integer> arlst = new ArrayList();
+        arlst.add("hello");
+        arlst.add(1);
+        arlst.add(2.3);
+        arlst.add('a');
+        int a = arlst.get(56); // this will give an error because the index is out of bound
+        System.out.println(a); // output : 1
+
     }
 }
