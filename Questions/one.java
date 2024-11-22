66f9132001a5c2849cf900b6e3aa687342e483a3
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-11-22 16:02:24 +0545

    Added a new Java class 'one' with a main method for a simple Hello World program; updated launch configuration in launch.json for the new class.

---
+++ Questions/one.java
@@ -0,0 +1,8 @@
+package Questions;
+
+public class one {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("hey how you doing");
+    }
+}
