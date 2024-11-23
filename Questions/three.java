03fbe94124c763de7e5c84e8ee66118f386cc0bf
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-11-23 12:45:04 +0545

    Added functionality to class 'two' for odd and even number output based on user input; created a new class 'three' as a placeholder.

---
+++ Questions/three.java
@@ -0,0 +1,7 @@
+package Questions;
+
+public class three {
+    public static void main(String[] args) {
+        
+    }
+}
--- Questions/two.java
+++ Questions/two.java
@@ -1,7 +1,40 @@
 package Questions;
 
+import java.util.Scanner;
+
 public class two {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+        for(int i = 1; i <= 10; i++) {
+            if (i %2 !=0) {
+                System.out.println(i);
+                // ouptut: 1, 3, 5, 7, 9
+            }
+        }
+
+        for (int i = 0; i <n; i++) {
+            if (i %2 !=0) {
+                System.out.println(i);
+                // ouptput will be all odd numbers from 0 to n
+                
+            }
+            else if (i %2 ==0) {
+                System.out.println(i);
+                // ouptput will be all even numbers from 0 to n
+            }
+            
+        }
+        for(int i = 1; i <=n; i ++)
+        {
+            if (i %2 == 0) {
+                System.out.println("now the even numbers are "+i);
+            }
+            else if(i % 2!= 0 ){
+                System.out.println(i+"is the odd numebrs ");
+            }
+        }
     }
 }
